0481d213c1baed6f172612a34df7996cda34b485
DannyDoneva96 <danielasinmas1@gmail.com>
2023-01-22 19:03:43 +0200

    start to write the logic

--- react-hangman/src/App.js
+++ react-hangman/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import Hangman from './Components/Hangman';
 import Header from './Components/Header';
@@ -5,13 +6,14 @@ import Word from './Components/Word';
 import WrongLetter from './Components/WrongLetter';
 
 function App() {
+  const [playable, setPlayable] = useState(true);
   return (
     <div className="App">
      <Header />
      <Hangman />
      <WrongLetter />
      <Word/>
-     
+
     </div>
   );
 }
