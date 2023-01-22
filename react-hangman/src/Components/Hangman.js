8f29e7b3dc64372e57b9cd24430e0f52753f2238
DannyDoneva96 <danielasinmas1@gmail.com>
2023-01-22 17:56:08 +0200

    up

---
+++ react-hangman/src/Components/Hangman.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hangman = () => {
+  return (
+    <div className="game-container">
+        <svg height="250" width="200" className='figure-container'>
+            <line x1='60' y1='20' x2='140' y2='20' />
+        </svg>
+      
+    </div>
+  )
+}
+
+export default Hangman
--- react-hangman/src/Components/Header.js
+++ react-hangman/src/Components/Header.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      
+      <h1>Hangman</h1>
+      <p>Find the hidden word - Enter a letter </p>
     </div>
   )
 }
