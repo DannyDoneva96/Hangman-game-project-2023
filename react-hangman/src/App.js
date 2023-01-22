fff7416e08823a142aca032bdaead4d2c645cf9a
DannyDoneva96 <danielasinmas1@gmail.com>
2023-01-22 19:19:13 +0200

    up

--- react-hangman/src/App.js
+++ react-hangman/src/App.js
@@ -2,24 +2,25 @@ import { useState } from 'react';
 import './App.css';
 import Hangman from './Components/Hangman';
 import Header from './Components/Header';
+import Popup from './Components/Popup';
 import Word from './Components/Word';
 import WrongLetter from './Components/WrongLetter';
 
-const words = ['application', 'programming', 'interface', 'wizard'];
+const words = ['application', 'programming', 'interface', 'wizard','zebra','developer','animal','JavaScript'];
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 function App() {
   const [playable, setPlayable] = useState(true);
-  const [correctLetter, setCorrectLetter] = useState([])
+  const [correctLetters, setCorrectLetters] = useState([])
   const [wrongLetters, setWrongLetters] = useState([])
 
   return (
     <div className="App">
      <Header />
-     <Hangman />
-     <WrongLetter />
-     <Word/>
-
+     <Hangman wrongLetters={wrongLetters}/>
+     <WrongLetter wrongLetters={wrongLetters} />
+     <Word selectedWord={selectedWord} correctLetters={correctLetters}/>
+      <Popup correctLetters={correctLetters} wrongLetters={wrongLetters} selectedWord={selectedWord} setPlayable={setPlayable}  />
     </div>
   );
 }
