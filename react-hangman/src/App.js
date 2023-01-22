d83af2a960b03532811cd38c421a022a5d4a6768
DannyDoneva96 <danielasinmas1@gmail.com>
2023-01-22 18:55:10 +0200

    UP

--- react-hangman/src/App.js
+++ react-hangman/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Hangman from './Components/Hangman';
 import Header from './Components/Header';
+import Word from './Components/Word';
+import WrongLetter from './Components/WrongLetter';
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <Hangman />
+     <WrongLetter />
+     <Word/>
     </div>
   );
 }
---
+++ react-hangman/src/Components/Word.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Word = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Word
