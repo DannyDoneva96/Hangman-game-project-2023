36eac5637f49dd11971e727f8a44f9bb56dc4bd5
DannyDoneva96 <danielasinmas1@gmail.com>
2023-01-22 18:58:24 +0200

    add more components structure

--- react-hangman/src/App.js
+++ react-hangman/src/App.js
@@ -11,6 +11,7 @@ function App() {
      <Hangman />
      <WrongLetter />
      <Word/>
+     
     </div>
   );
 }
---
+++ react-hangman/src/Components/Popup.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Popup = () => {
+  return (
+    <div className="popup-container" >
+    <div className="popup">
+      
+      <button >Play Again</button>
+    </div>
+  </div>
+  )
+}
+
+export default Popup
