cca85f886d0d192012df8bb0d552863cd6700c1a
DannyDoneva96 <danielasinmas1@gmail.com>
2023-01-22 17:59:26 +0200

    Update Hangman.js

--- react-hangman/src/Components/Hangman.js
+++ react-hangman/src/Components/Hangman.js
@@ -5,6 +5,20 @@ const Hangman = () => {
     <div className="game-container">
         <svg height="250" width="200" className='figure-container'>
             <line x1='60' y1='20' x2='140' y2='20' />
+            <line x1='140' y1='20' x2='140' y2='50' />
+            <line x1='60' y1='20' x2='60' y2='230' />
+            <line x1='20' y1='230' x2='100' y2='230' />
+
+            {/* head */}
+
+            <circle cx='140' cy='70' r='20' className='figure-part' />
+
+             {/* body */}
+
+             <line x1='140' y1='90' x2='140' y2='150' className='figure-part' />
+
+
+
         </svg>
       
     </div>
